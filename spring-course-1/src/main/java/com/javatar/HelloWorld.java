7f2784a0a24e5a582a670cd32d52ec3b1badb50f
e.sadeghi <e.sadeghi@fwutech.com>
2023-06-02 06:11:19 -0400

    first commit

---
+++ spring-course-1/src/main/java/com/javatar/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.javatar;
+
+public class HelloWorld {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
